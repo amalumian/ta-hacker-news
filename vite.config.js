549a7afee8fb574cbf4187318d182fcf5c92ddfb
Arthur Malumian <amalumian@outlook.com>
2024-08-26 21:17:03 +0700

    Add test setup

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
