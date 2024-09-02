ebf97aa4e8b5c9741f5c84580a726da5eefea638
Arthur Malumian <amalumian@outlook.com>
2024-09-02 23:12:07 +0700

    Remove unnecessary import

--- src/pages/StoryPage/StoryPage.test.jsx
+++ src/pages/StoryPage/StoryPage.test.jsx
@@ -1,4 +1,4 @@
-import { getByTestId, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { describe, expect, it, vi } from 'vitest'
 
 import StoryPage from './index'
