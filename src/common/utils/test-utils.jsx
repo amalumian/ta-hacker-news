6b9f10ec04af60d0f35f18304b4ea5b53b261b22
Arthur Malumian <amalumian@outlook.com>
2024-09-03 04:22:58 +0700

    Add test-utils.jsx

---
+++ src/common/utils/test-utils.jsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+
+import store from '@/app/store'
+
+export function renderWithProviders(ui) {
+  const Wrapper = ({ children }) => <Provider store={store}>{children}</Provider>
+
+  return {
+    store,
+    ...render(ui, { wrapper: Wrapper }),
+  }
+}
+
+export function renderWithRouterProviders(ui, { id } = {}) {
+  const Wrapper = ({ children }) => (
+    <Provider store={store}>
+      <MemoryRouter initialEntries={[`/story/${id}`]}>
+        <Routes>
+          <Route path='/story/:id' element={children} />
+        </Routes>
+      </MemoryRouter>
+    </Provider>
+  )
+
+  return {
+    store,
+    ...render(ui, { wrapper: Wrapper }),
+  }
+}
