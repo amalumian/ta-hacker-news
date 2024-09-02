827974866ccf21b856288791db7e246ce515d6c5
Arthur Malumian <amalumian@outlook.com>
2024-09-02 17:46:53 +0700

    Add tests for ErrorMessage

---
+++ src/common/components/ErrorMessage/ErrorMessage.test.jsx
@@ -0,0 +1,21 @@
+import { describe, expect, test } from 'vitest'
+import { render, screen, within } from '@testing-library/react'
+
+import ErrorMessage from './index'
+
+describe('ErrorMessage Component', () => {
+  test('renders with correct message', () => {
+    const errorMessage = 'Error loading story'
+    render(<ErrorMessage message={errorMessage} />)
+
+    const alertElement = screen.getByRole('alert')
+    const alertMessage = within(alertElement).getByText(errorMessage)
+
+    expect(alertMessage).toBeInTheDocument()
+  })
+
+  test('matches snapshot', () => {
+    const { container } = render(<ErrorMessage message='Snapshot test' />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
