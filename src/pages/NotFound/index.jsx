ec942d40fce281730e4ae418c40b0ed26f3c5f5a
Arthur Malumian <amalumian@outlook.com>
2024-09-04 01:21:23 +0700

    Add ButtonBack to NotFound page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,15 +1,17 @@
-import { Typography } from 'antd'
+import { Typography, Space } from 'antd'
 
 import ErrorMessage from '@/common/components/ErrorMessage'
+import ButtonBack from '@/common/components/ButtonBack'
 
 const { Title } = Typography
 
 const NotFound = () => {
   return (
-    <>
+    <Space direction='vertical' style={{ width: '100%' }}>
       <Title level={1}>404</Title>
+      <ButtonBack />
       <ErrorMessage message='Page not found' />
-    </>
+    </Space>
   )
 }
 
